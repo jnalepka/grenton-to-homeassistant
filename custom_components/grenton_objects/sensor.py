0aeafa45a3b16c3406496938700e13fa6e42eb66
Jan Nalepka <70645322+jnalepka@users.noreply.github.com>
2024-04-23 21:58:51 +0200

    Create sensor.py

---
+++ custom_components/grenton_objects/sensor.py
@@ -0,0 +1,70 @@
+import requests
+import logging
+import json
+import voluptuous as vol
+from homeassistant.components.sensor import (
+    SensorEntity,
+    PLATFORM_SCHEMA,
+    SensorDeviceClass
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = 'grenton_objects'
+
+CONF_API_ENDPOINT = 'api_endpoint'
+CONF_GRENTON_ID = 'grenton_id'
+CONF_OBJECT_NAME = 'name'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_API_ENDPOINT): str,
+    vol.Required(CONF_GRENTON_ID): str,
+    vol.Optional(CONF_OBJECT_NAME, default='Grenton Sensor'): str
+})
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    api_endpoint = config.get(CONF_API_ENDPOINT)
+    grenton_id = config.get(CONF_GRENTON_ID)
+    object_name = config.get(CONF_OBJECT_NAME)
+
+    add_entities([GrentonSensor(api_endpoint, grenton_id, object_name)], True)
+
+class GrentonSensor(SensorEntity):
+    def __init__(self, api_endpoint, grenton_id, object_name):
+        self._device_class = SensorDeviceClass.TEMPERATURE
+        self._api_endpoint = api_endpoint
+        self._grenton_id = grenton_id
+        self._object_name = object_name
+        self._unique_id = f"grenton_{grenton_id.split('->')[1]}"
+        self._native_value = None
+        self._native_unit_of_measurement = '°C'
+
+    @property
+    def name(self):
+        return self._object_name
+
+    @property
+    def unique_id(self):
+        return self._unique_id
+
+    @property
+    def native_value(self):
+        return self._native_value
+    
+    @property
+    def native_unit_of_measurement(self):
+        return self._native_unit_of_measurement
+
+    def update(self):
+        try:
+            command = {"status": f"return {self._grenton_id.split('->')[0]}:execute(0, '{self._grenton_id.split('->')[1]}:get(0)')"}
+            response = requests.get(
+                f"{self._api_endpoint}",
+                json = command
+            )
+            response.raise_for_status()
+            data = response.json()
+            self._native_value = data.get("status")
+        except requests.RequestException as ex:
+            _LOGGER.error(f"Failed to update the switch state: {ex}")
+            self._state = None
