7708798aa1a3d139e7c836894c97e20ab46e1892
Jan Nalepka <70645322+jnalepka@users.noreply.github.com>
2024-10-11 11:35:47 +0200

    Update __init__.py

--- custom_components/grenton_objects/__init__.py
+++ custom_components/grenton_objects/__init__.py
@@ -1,1 +1,34 @@
 """Grenton objects integration by Jan Nalepka."""
+
+async def async_setup_entry(hass, config_entry):
+    """Set up Grenton from a config entry."""
+    
+    devices = config_entry.data["devices"]
+
+    for device in devices:
+        if device["device_type"] == "light":
+            hass.async_create_task(
+                hass.config_entries.async_forward_entry_setup(config_entry, "light")
+            )
+        elif device["device_type"] == "switch":
+            hass.async_create_task(
+                hass.config_entries.async_forward_entry_setup(config_entry, "switch")
+            )
+        elif device["device_type"] == "climate":
+            hass.async_create_task(
+                hass.config_entries.async_forward_entry_setup(config_entry, "climate")
+            )
+        elif device["device_type"] == "cover":
+            hass.async_create_task(
+                hass.config_entries.async_forward_entry_setup(config_entry, "cover")
+            )
+        elif device["device_type"] == "sensor":
+            hass.async_create_task(
+                hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+            )
+        elif device["device_type"] == "binary_sensor":
+            hass.async_create_task(
+                hass.config_entries.async_forward_entry_setup(config_entry, "binary_sensor")
+            )
+
+    return True
