5d2be1eef3a3386ee7fb571a633b9af314d0680a
Jan Nalepka <70645322+jnalepka@users.noreply.github.com>
2024-10-20 13:33:48 +0200

    Update __init__.py

--- custom_components/grenton_objects/__init__.py
+++ custom_components/grenton_objects/__init__.py
@@ -17,18 +17,15 @@ async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     return True
 
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
-    """Set up Grenton objects from a config entry."""
     device = config_entry.data
     platform = device["device_type"]
-    await hass.config_entries.async_forward_entry_setups(config_entry, {platform})
-
+    if platform:
+        await hass.config_entries.async_forward_entry_setups(config_entry, {platform})
     return True
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     device_type = config_entry.data.get("device_type")
-    unload_ok = await hass.config_entries.async_unload_platforms(config_entry, [device_type])
-    
-    if unload_ok:
-        hass.data[DOMAIN].pop(config_entry.entry_id, None)
-    
-    return unload_ok
+    if device_type:
+        unload_ok = await hass.config_entries.async_unload_platforms(config_entry, [device_type])
+        return unload_ok
+    return False
