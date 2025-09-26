8d6ac60eb114fd619b6449555f40a6f8ed4af890
Plejader <70645322+jnalepka@users.noreply.github.com>
2025-09-27 00:29:28 +0200

    new tests

--- tests/test_binary_sensor_logic.py
+++ tests/test_binary_sensor_logic.py
@@ -2,20 +2,20 @@ import pytest
 from custom_components.grenton_objects.binary_sensor import GrentonBinarySensor
 from homeassistant.const import STATE_ON, STATE_OFF
 
-def create_sensor(grenton_id="CLU220000000->DIN0000", status=1, captured_command=None):
-    sensor = GrentonBinarySensor(
+def create_obj(grenton_id="CLU220000000->DIN0000", status=1, captured_command=None):
+    obj = GrentonBinarySensor(
         api_endpoint="http://fake-api",
         grenton_id=grenton_id,
         object_name="Test Sensor",
         auto_update=False,
         update_interval=5
     )
-    sensor._initialized = True
+    obj._initialized = True
 
     class MockHass:
         def async_add_job(self, *args, **kwargs): pass
-    sensor.hass = MockHass()
-    sensor.async_write_ha_state = lambda: None
+    obj.hass = MockHass()
+    obj.async_write_ha_state = lambda: None
 
     class FakeResponse:
         async def json(self): return {"status": status}
@@ -31,32 +31,32 @@ def create_sensor(grenton_id="CLU220000000->DIN0000", status=1, captured_command
                 captured_command["value"] = json
             return FakeResponse()
 
-    return sensor, FakeSession
+    return obj, FakeSession
 
 @pytest.mark.asyncio
 async def test_async_update(monkeypatch):
     captured_command = {}
-    sensor, FakeSession = create_sensor(status=1, captured_command=captured_command)
+    obj, FakeSession = create_obj(status=1, captured_command=captured_command)
     monkeypatch.setattr("aiohttp.ClientSession", lambda: FakeSession())
-    await sensor.async_update()
+    await obj.async_update()
 
     assert captured_command["value"] == {
         "status": "return CLU220000000:execute(0, 'DIN0000:get(0)')"
     }
-    assert sensor._state == STATE_ON
-    assert sensor.is_on is True
-    assert sensor.unique_id == "grenton_DIN0000"
+    assert obj._state == STATE_ON
+    assert obj.is_on is True
+    assert obj.unique_id == "grenton_DIN0000"
 
 @pytest.mark.asyncio
 async def test_async_update_off(monkeypatch):
     captured_command = {}
-    sensor, FakeSession = create_sensor(status=0, captured_command=captured_command)
+    obj, FakeSession = create_obj(status=0, captured_command=captured_command)
     monkeypatch.setattr("aiohttp.ClientSession", lambda: FakeSession())
-    await sensor.async_update()
+    await obj.async_update()
 
     assert captured_command["value"] == {
         "status": "return CLU220000000:execute(0, 'DIN0000:get(0)')"
     }
-    assert sensor._state == STATE_OFF
-    assert sensor.is_on is False
-    assert sensor.unique_id == "grenton_DIN0000"
+    assert obj._state == STATE_OFF
+    assert obj.is_on is False
+    assert obj.unique_id == "grenton_DIN0000"
--- tests/test_climate_logic.py
+++ tests/test_climate_logic.py
@@ -1,12 +1,17 @@
 import pytest
-from custom_components.grenton_objects.button import GrentonScript
+from custom_components.grenton_objects.climate import GrentonClimate
+from homeassistant.const import STATE_ON, STATE_OFF
+from homeassistant.components.climate import HVACMode
 
-def create_obj(grenton_id="my_script", status="ok", captured_command=None):
-    obj = GrentonScript(
+def create_obj(grenton_id="CLU220000000->THE0000", status="ok", captured_command=None):
+    obj = GrentonClimate(
         api_endpoint="http://fake-api",
         grenton_id=grenton_id,
-        object_name="Test Script"
+        object_name="Test obj",
+        auto_update=False,
+        update_interval=5
     )
+    obj._initialized = True
 
     class MockHass:
         def async_add_job(self, *args, **kwargs): pass
@@ -14,6 +19,7 @@ def create_obj(grenton_id="my_script", status="ok", captured_command=None):
     obj.async_write_ha_state = lambda: None
 
     class FakeResponse:
+        async def json(self): return {"status": status}
         def raise_for_status(self): pass
         async def __aenter__(self): return self
         async def __aexit__(self, *args): pass
@@ -28,25 +34,12 @@ def create_obj(grenton_id="my_script", status="ok", captured_command=None):
     return obj, FakeSession
 
 @pytest.mark.asyncio
-async def test_async_script_local(monkeypatch):
+async def test_async_set_temperature(monkeypatch):
     captured_command = {}
     obj, FakeSession = create_obj(status="ok", captured_command=captured_command)
     monkeypatch.setattr("aiohttp.ClientSession", lambda: FakeSession())
-    await obj.async_press()
+    await obj.async_update()
 
     assert captured_command["value"] == {
-        "command": "my_script(nil)"
+        "command": "CLU220000000:execute(0, 'THE0000:set(8, 0)')", "command_2": f"CLU220000000:execute(0, 'THE0000:set(3, 20)')"
     }
-    assert obj.unique_id == "grenton_my_script"
-
-@pytest.mark.asyncio
-async def test_async_script_remote(monkeypatch):
-    captured_command = {}
-    obj, FakeSession = create_obj(grenton_id="CLU220000000->my_script_2", status="ok", captured_command=captured_command)
-    monkeypatch.setattr("aiohttp.ClientSession", lambda: FakeSession())
-    await obj.async_press()
-
-    assert captured_command["value"] == {
-        "command": "CLU220000000:execute(0, 'my_script_2(nil)')"
-    }
-    assert obj.unique_id == "grenton_my_script_2"
