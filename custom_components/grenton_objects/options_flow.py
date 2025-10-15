eedbee0ca56b20795a398db6bce7f1cc289c8daf
Plejader <70645322+jnalepka@users.noreply.github.com>
2025-10-16 01:02:53 +0200

    add reversed cover options flow

--- custom_components/grenton_objects/options_flow.py
+++ custom_components/grenton_objects/options_flow.py
@@ -1,8 +1,8 @@
 """
 ==================================================
 Author: Jan Nalepka
-Script version: 3.0
-Date: 15.09.2025
+Script version: 3.1
+Date: 16.10.2025
 Repository: https://github.com/jnalepka/grenton-to-homeassistant
 ==================================================
 """
@@ -13,10 +13,14 @@ from .const import (
     CONF_API_ENDPOINT,
     CONF_AUTO_UPDATE,
     CONF_UPDATE_INTERVAL, 
-    DEFAULT_UPDATE_INTERVAL
+    DEFAULT_UPDATE_INTERVAL,
+    CONF_REVERSED
 )
 
 class GrentonOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry: config_entries.ConfigEntry):
+        self.config_entry = config_entry
+        self.device_type = config_entry.data.get("device_type", "")
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
@@ -39,4 +43,11 @@ class GrentonOptionsFlowHandler(config_entries.OptionsFlow):
             vol.Required(CONF_AUTO_UPDATE, default=default_auto_update): bool,
             vol.Required(CONF_UPDATE_INTERVAL, default=default_update_interval): vol.All(vol.Coerce(int), vol.Range(min=5, max=3600))
         })
+
+        if self.device_type == "cover":
+            default_reversed = self.config_entry.options.get(CONF_REVERSED, False)
+            data_schema = data_schema.extend({
+                vol.Required(CONF_REVERSED, default=default_reversed): bool
+            })
+
         return self.async_show_form(step_id="init", data_schema=data_schema)
